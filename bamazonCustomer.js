d2093303c2301bbb314ccfec9941aa053cc8ad6c
Mapi Pina <mpina09@gmail.com>
2018-04-15 14:50:24 -0400

    major errors fixed. just have to update database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,24 +10,13 @@ let connection = mysql.createConnection({
 
 connection.connect(function(err){
 	if (err) throw err;
-	console.log(`Connection is: ${connection.threadID}`);
+	console.log("connected as id " + connection.threadId);
+	shopMode();
 });
-
-function readDB () {
-	connection.query("SELECT ? FROM products WHERE ?",
-		{
-		stock_quantity: answer.amount,
-		item_id: answer.item
-		},
-		function(err, res) {
-			if (err) throw err;
-			console.log(res);
-			connection.end();
-		});
-};
 
 function shopMode(){
 	inquirer.prompt(
+	[
 	{
 		type: "input",
 		message: "Type ID of item you want to purchase",
@@ -35,10 +24,65 @@ function shopMode(){
 	},
 	{
 		type: "input",
-		message: "How many items?"
+		message: "How many items?",
 		name: "amount"
 	}
-	).then(function(answer){
-		readDB();
-	})
+	])
+	.then(function(answer){
+		connection.query(
+			"UPDATE products SET ? WHERE ?",
+			[{
+				requested: answer.amount
+			},
+			{
+				item_id: answer.item
+			}],
+			function(err, res){
+				if (err) throw err;
+			});
+		connection.query(
+			"SELECT stock_quantity, requested FROM products WHERE ?",
+			{
+				item_id: answer.item
+			},
+			function(err, res) {
+				if (err) {
+					console.log("Error message is " + err)
+				} else if (res[0].stock_quantity < res[0].requested)
+				{
+					console.log("Insufficient amount!")
+				} else {
+					console.log(`There are ${res[0].stock_quantity} left`);
+				}
+			});
+	});
 };
+
+// function amount(){
+// 	connection.query(
+// 		"UPDATE products SET ? WHERE ?",
+// 		[{
+// 			requested: answer.amount
+// 		},
+// 		{
+// 			item_id: answer.item
+// 		}],
+// 		function(err, res){
+// 			if (err) throw err;
+// 		});
+// };
+
+// function readDB () {
+// 	connection.query(
+// 		"SELECT stock_quantity FROM products WHERE ?",
+// 		{
+// 			item_id: answer.item
+// 		},
+// 		function(err, res) {
+// 			if (err) throw err;
+// 			console.log(`There are ${res[0].stock_quantity} left`)
+// 		});
+// };
+
+
+
